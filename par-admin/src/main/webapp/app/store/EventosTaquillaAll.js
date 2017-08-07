53598033d74d7c01b4d55b0fc32cdce2dc04abe4
Antonio Eugenio Burriel <aeburriel@gmail.com>
2017-08-07 22:26:36 +0200

    En "Compras y Reservas" ordenamos los espectáculos por fecha de primera sesión descendente.

--- par-admin/src/main/webapp/app/store/EventosTaquillaAll.js
+++ par-admin/src/main/webapp/app/store/EventosTaquillaAll.js
@@ -2,6 +2,10 @@ Ext.define('Paranimf.store.EventosTaquillaAll', {
    extend: 'Ext.data.Store',
    model: 'Paranimf.model.Evento',
 
+   sorters: [{
+      property: 'fechaPrimeraSesion',
+      direction: 'DESC'
+   }],
    autoLoad: false,
    autoSync: false,
    pageSize: 20,
