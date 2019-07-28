5eabbe63500e82bbd018d0096bb73a2b3d363f81
Antonio Eugenio Burriel <aeburriel@gmail.com>
2019-07-28 14:00:50 +0200

    Actualizamos literales
    Disponibilidad de butacas accesibles hasta 48 horas antes del comienzo
    de la sesión.

--- uji-par-web-benicassim/src/main/webapp/js/i18n/es.js
+++ uji-par-web-benicassim/src/main/webapp/js/i18n/es.js
@@ -3,11 +3,11 @@ UI =
     "i18n": {
 		"informativo": {
 			"titulo": "Información",
-			"cuerpo": "<p>Las 4 butacas para personas en silla de ruedas y de su acompañante estarán a la venta on-line hasta 1 hora y media antes del espectáculo; la compra se realizará simultaneamente.</p><p>Una vez pasado el plazo de la venta on-line, las butacas no vendidas para personas en silla de ruedas se pondrán a la venta en taquilla.</p><p>Cuando se dé otro tipo de diversidad funcional deberán ponerse en contacto en el 964300962, ext. 248 y 268 en horario de 09:30 a 13:30 de lunes a viernes.</p>"
+			"cuerpo": "<p>Las 4 butacas para personas en silla de ruedas y de su acompañante estarán a la venta on-line hasta 48 horas antes del espectáculo; la compra se realizará simultaneamente.</p><p>Una vez pasado el plazo de la venta on-line, las butacas no vendidas para personas en silla de ruedas se pondrán a la venta en taquilla.</p><p>Cuando se dé otro tipo de diversidad funcional deberán ponerse en contacto en el 964300962, ext. 248 y 268 en horario de 09:30 a 13:30 de lunes a viernes.</p>"
 		},
         "butacas": {
             "acompañante": "Solo disponible para acompañante, seleccionar primero entrada silla de ruedas.",
-            "discapacitado": "Las 4 butacas para personas en silla de ruedas y de su acompañante estarán a  la venta on-line hasta 1 hora y media antes del espectáculo. La compra se realizará simultaneamente.",
+            "discapacitado": "Las 4 butacas para personas en silla de ruedas y de su acompañante estarán a  la venta on-line hasta 48 horas antes del espectáculo. La compra se realizará simultaneamente.",
             "ocupadas": "La butaca seleccionada no está disponible",
             "localizacion": "Localización",
             "fila": "F.",
