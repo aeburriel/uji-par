f9cf405608d252cc0bbe1f0c6a226a4cb42ce255
Antonio Eugenio Burriel <aeburriel@gmail.com>
2021-06-10 11:50:48 +0200

    Rectificamos índice único
    Arregla el commit 34915f2d3ff7b157682f7c6ac420e8ccc114c363.

--- uji-par-base/src/main/resources/database/postgres/2021-05-12.sql
+++ uji-par-base/src/main/resources/database/postgres/2021-05-12.sql
@@ -1,4 +1,4 @@
 ALTER TABLE par_plantillas DROP CONSTRAINT par_plantillas_nombre_key;
-ALTER TABLE par_plantillas ADD CONSTRAINT par_plantillas_nombre_key UNIQUE (nombre, id);
+ALTER TABLE par_plantillas ADD CONSTRAINT par_plantillas_nombre_key UNIQUE (nombre, sala_id);
 
 INSERT INTO par_version_bbdd (VERSION) VALUES ('2021-05-12.SQL');
